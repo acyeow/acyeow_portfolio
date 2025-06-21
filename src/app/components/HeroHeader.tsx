1dc6fcc973331f4b99e4b068d8edad9f64767f96
acyeow <acyeow@ucdavis.edu>
2025-06-21 04:22:09 -0700

    chore: cleanup heao header

--- src/app/components/HeroHeader.tsx
+++ src/app/components/HeroHeader.tsx
@@ -4,7 +4,7 @@ import HeroProjects from "./HeroProjects";
 
 const HeroHeader = () => {
   return (
-    <div className="flex flex-col md:flex-row p-4 text-zinc-600 text-[0.7rem] space-y-6 md:space-y-0">
+    <div className="p-4 text-zinc-600 text-[0.7rem]">
       <div className="w-full md:w-1/3">
         ABOUT
         <div className="p-1"></div>
@@ -14,10 +14,8 @@ const HeroHeader = () => {
         <div className="p-1"></div>
       </div>
       <div className="w-full md:w-1/2">
-        <div className="flex">
-          <div className="w-1/3">PROJECTS</div>
-          <div className="w-2/3">DESCRIPTION</div>
-        </div>
+        <div className="w-1/3">PROJECTS</div>
+        <div className="w-2/3">DESCRIPTION</div>
         <div className="p-1"></div>
         <HeroProjects />
       </div>
