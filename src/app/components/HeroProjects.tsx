13b1ec4ac8e357b33b84b070d9c9889108b82dee
acyeow <acyeow@ucdavis.edu>
2025-06-21 04:57:53 -0700

    chore: updated font to be consistent accross app

--- src/app/components/HeroProjects.tsx
+++ src/app/components/HeroProjects.tsx
@@ -3,12 +3,12 @@ import { timelineProjects } from "../data/projects";
 
 const HeroProjects = () => {
   return (
-    <div className="space-y-4 text-gray-200 text-[0.85rem] font-semibold">
+    <div className="text-gray-200 text-[0.80rem] font-semibold">
       {timelineProjects.map((project, index) => (
         <div key={index} className="flex items-start">
           <div className="w-1/3">
             <div>{project.title}</div>
-            <div className="font-medium text-gray-200 text-[0.70rem] uppercase">
+            <div className="text-gray-200 text-[0.70rem] font-medium uppercase">
               {project.date}
             </div>
           </div>
